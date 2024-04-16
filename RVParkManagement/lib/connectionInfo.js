bdb29db19fa7cc2d51e64e43d7fdf114a9aa4247
isaacporter1 <112592768+isaacporter1@users.noreply.github.com>
2024-04-16 12:56:28 -0600

    Adding beginning Procedures

--- RVParkManagement/lib/connectionInfo.js
+++ RVParkManagement/lib/connectionInfo.js
@@ -1,18 +1,7 @@
 var connectionInfo = {
-<<<<<<< HEAD
   host: "127.0.0.1",
   port: 3306,
   user: "isaac",
   password: "isaac"
 };
 module.exports = connectionInfo;
-//testtest
-=======
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "Cs3750$pass"
-  };
-  module.exports = connectionInfo;
-  //testtest
->>>>>>> origin/master
