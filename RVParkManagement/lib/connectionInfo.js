697b33e133f52f6f9d097b5bc48daaadcac95a72
Alon Saar <97154163+130on@users.noreply.github.com>
2024-04-18 18:22:23 -1000

    updated index and home

--- RVParkManagement/lib/connectionInfo.js
+++ RVParkManagement/lib/connectionInfo.js
@@ -1,10 +1,10 @@
 var connectionInfo = {
   host: "127.0.0.1",
   port: 3306,
-  user: "isaac",
-  //user: "AlonCS3750",
-  password: "isaac"
-  //password: "AlonCS3750"
+  //user: "isaac",
+  user: "AlonCS3750",
+  //password: "isaac"
+  password: "AlonCS3750"
 };
 module.exports = connectionInfo;
 //testtest
