ef58fbb816fe52f2ff3adfcf74dc0a4d5e391d1b
isaacporter1 <112592768+isaacporter1@users.noreply.github.com>
2024-04-20 01:20:01 -0600

    whole lotta crap but it works

--- RVParkManagement/routes/payment.js
+++ RVParkManagement/routes/payment.js
@@ -1,15 +1,96 @@
 var express = require('express');
 var router = express.Router();
 
+var dbCon = require('../lib/database');
+
+// Function to format a date as mm/dd/yyyy
+function formatDate(date) {
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getFullYear();
+  return year + '-' + month + '-' + day;
+}
+
+// Function to parse yyyy-mm-dd formatted date strings into Date objects
+function parseDate(dateString) {
+  console.log(dateString);
+  const parts = dateString.split('-');
+  const year = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10);
+  const day = parseInt(parts[2], 10);
+  return new Date(year, month - 1, day); // Month in JavaScript Date object is 0-based
+}
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log("payment.js: GET");
-  res.render('payment', { });
+  res.render('payment', {});
+
+  var userId;
+  let sql = "SELECT user_id\n" +
+    "FROM users\n" +
+    "WHERE username = (?);\n";
+
+  dbCon.query(sql, [req.session.username], function (err, results) {
+    if (err) {
+      console.log("index.js: Query to find userId failed");
+      throw err;
+    }
+    if (results.length > 0) {
+      userId = parseInt(results[0].user_id);
+      req.session.userId = userId;
+      console.log("loginuser.js: The userId is: ", userId);
+    }
+  });
+
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   console.log("payment.js: POST");
-  res.redirect('/confirmation');
+
+  const siteId = req.query.site_id;
+  const reservationType = req.query.type;
+  var size = req.query.rvSize;
+  if (size == '') {
+    size = null;
+  }
+  const pricePerNight = req.query.pricePerNight;
+  const fromDate = req.query.fromDate;
+  const toDate = req.query.toDate;
+
+  const cardNumber = req.body.cardNumber;
+
+  const fromDateObj = parseDate(fromDate);
+  const toDateObj = parseDate(toDate);
+  const oneDay = 24 * 60 * 60 * 1000;
+  const daysDifference = Math.round(Math.abs((fromDateObj - toDateObj) / oneDay));
+  const amount = daysDifference * pricePerNight;
+
+  const todaysDate = formatDate(new Date());
+
+  console.log("Parameters:", siteId, size, pricePerNight, fromDate, toDate);
+
+  sql = "CALL make_payment(?, ?, ?, ?, ?, ?, @result); SELECT @result";
+  dbCon.query(sql, [cardNumber, amount, todaysDate, 'Active', 'New Reservation', req.session.userId], function (err, results) {
+    if (err) {
+      throw err;
+    }
+    const paymentId = results[1][0]['@result'];
+    console.log("Payment ID:", paymentId);
+
+    sql = "CALL create_reservation(?, ?, ?, ?, ?, ?, ?, ?, ?, @result); SELECT @result";
+    dbCon.query(sql, [req.session.userId, reservationType, siteId, paymentId, size, todaysDate, 'Active', fromDate, toDate], function (err, results) {
+      if (err) {
+        throw err;
+      }
+      const reservationId = results[1][0]['@result'];
+      console.log("Reservation ID:", reservationId);
+
+      res.redirect('confirmation?reservation_id=' + reservationId)
+    });
+  });
 });
 
 module.exports = router;
+
--- RVParkManagement/routes/reservation.js
+++ RVParkManagement/routes/reservation.js
@@ -4,16 +4,37 @@ var router = express.Router();
 var dbCon = require('../lib/database');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log("reservation.js: POST");
 
   const reservationType = req.query.type;
-  const size = req.query.rvSize;
+  var size = req.query.rvSize;
   const pricePerNight = req.query.pricePerNight;
   const fromDate = req.query.fromDate;
   const toDate = req.query.toDate;
 
+  if (size == '') {
+    size = null;
+  }
+
   console.log("Parameters:", reservationType, size, pricePerNight, fromDate, toDate);
+
+  var userId;
+  let sql = "SELECT user_id\n" +
+    "FROM users\n" +
+    "WHERE username = (?);\n";
+
+  dbCon.query(sql, [req.session.username], function (err, results) {
+    if (err) {
+      console.log("index.js: Query to find userId failed");
+      throw err;
+    }
+    if (results.length > 0) {
+      userId = parseInt(results[0].user_id);
+      req.session.userId = userId;
+      console.log("loginuser.js: The userId is: ", userId);
+    }
+  });
 
   sql = "CALL check_availability(?, ?, ?, ?, ?);";
   dbCon.query(sql, [reservationType, size, pricePerNight, fromDate, toDate], function (err, openSiteReservations) {
@@ -24,7 +45,7 @@ router.get('/', function(req, res, next) {
     if (openSiteReservations.length > 0) {
       const siteReservations = openSiteReservations[0];
       console.log("reservation.js: these are the open reservations: ", siteReservations);
-      res.render('reservation', { siteReservations: siteReservations, fromDate, toDate });
+      res.render('reservation', { siteReservations: siteReservations, size, fromDate, toDate, reservationType });
     }
     else {
       console.log("reservation.js: there are no open reservations with these parameters.");
@@ -33,10 +54,17 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   console.log("reservation.js: POST");
 
-  res.redirect('/payment');
+  const siteId = req.body.site_id;
+  const reservationType = req.body.reservation_type;
+  const size = req.body.size;
+  const pricePerNight = req.body.pricePerNight;
+  const fromDate = req.body.fromDate;
+  const toDate = req.body.toDate;
+
+  res.redirect('/payment?site_id=' + siteId + '&type=' + reservationType + '&rvSize=' + size + '&pricePerNight=' + pricePerNight + '&fromDate=' + fromDate + '&toDate=' + toDate);
 });
 
 module.exports = router;
