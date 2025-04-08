0a583040d04da61786f280a3f59122c85aa9e947
Alon Saar <97154163+130on@users.noreply.github.com>
2025-04-08 13:11:43 -1000

    created a .env file and removed DB creditials

--- RVParkManagement/lib/connectionInfo.js
+++ RVParkManagement/lib/connectionInfo.js
@@ -1,11 +1,19 @@
-var connectionInfo = {
-  host: "127.0.0.1",
-  port: 3306,
-  user: "isaac",
-  //user: "AlonCS3750",
-  password: "isaac"
-  //password: "AlonCS3750"
+// var connectionInfo = {
+//   host: "***.*.*.*",
+//   port: ****,
+//   user: "******",
+//   password: "********"
+// };
+
+// module.exports = connectionInfo;
+
+
+const connectionInfo = {
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER || 'your_default_user',
+  password: process.env.DB_PASSWORD || 'your_default_password',
+  // database: process.env.DB_NAME || 'your_db_name'
 };
 
 module.exports = connectionInfo;
-//testtest
